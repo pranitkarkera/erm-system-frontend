230b6e125ca2cb33c98a7e8c346688129f2ff970
Pranit Karkera <pranitkarkera99@gmail.com>
2025-06-04 18:46:29 +0530

    update5

--- src/store.ts
+++ src/store.ts
@@ -24,7 +24,7 @@ export interface StoreState {
 }
 
 // Configure axios defaults
-axios.defaults.baseURL = 'http://localhost:5000';
+axios.defaults.baseURL = import.meta.env.VITE_API_URL;;
 
 export const useStore = create<StoreState>((set) => ({
   user: null,
